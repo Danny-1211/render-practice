e6e4424a136a904dff6310df363d69fe1ba3ff6c
danny <danny21316@gmail.com>
2026-02-12 16:57:10 +0800

    fix response  err

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     // 如果前面是寫相對路徑,那我們需要跟 express 說明確的根目錄是哪一個
-    res.sendFile('./views/index.html', { root: __dirname });
+    // res.sendFile('./views/index.html', { root: __dirname });
     res.json({
         message: "test work!!!"
     })
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    // res.sendFile('./views/about.html', { root: __dirname });
     res.json({
         message: "about work!!!"
     })
