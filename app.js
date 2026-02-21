a842dddb9f6af640d7aa92e80248c4f7ac16b4e3
danny <danny21316@gmail.com>
2026-02-21 15:27:01 +0800

    using ejc example

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
+const engine = require('ejs-locals');
+app.engine('ejs', engine);
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    // 這樣寫是正確的，一次只回傳一個 JSON
-    res.json({
-        message: "test work!!!"
-    });
+    res.render('index')
 });
 
 app.get('/about', (req, res) => {
-    res.json({
-        message: "about work!!!"
-    });
+    res.render('about')
 });
 
-// redirect
-app.get('/about-me', (req, res) => {
-    res.redirect('/about');
-});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+})
 
 // 404
-// 注意：如果你的專案裡沒有 ./views/404.html 這個檔案，這裡還是會報錯導致崩潰
 app.use((req, res) => {
-    res.status(404).json({ error: "Page not found" }); 
-    // 建議先用 JSON 測試，確定通了再換回 sendFile
+    res.status(404).render('404')
 });
 
-// 關鍵修改：加上 '0.0.0.0'
-app.listen(port, '0.0.0.0', () => {   
+app.listen(port, '0.0.0.0', () => {
     console.log(`範例應用程式正在監聽連接埠 ${port}`);
 });
