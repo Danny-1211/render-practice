31f8dea75e31b43c0f1e26c722f56c72d6eee2af
danny <danny21316@gmail.com>
2026-02-23 15:47:45 +0800

    feat: passing into views

--- app.js
+++ app.js
@@ -7,21 +7,32 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'home',
+        titleH2: '<h2>第二級標題</h2>',
+        'show': true,
+        'foods': ['apple', 'banana', 'mongo']
+    })
 });
 
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about', {
+        title: 'about',
+    })
 });
 
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {
+        title: 'create',
+    });
 })
 
 // 404
 app.use((req, res) => {
-    res.status(404).render('404')
+    res.status(404).render('404', {
+        title: '404',
+    })
 });
 
 app.listen(port, '0.0.0.0', () => {
